7e89f7edb2ff7dff3109cbc513acf78881949f44
Aleksandar16 <a.milenkovic321@gmail.com>
2023-01-27 21:54:41 +0100

    Logs dix

--- api/src/Controller/LoginController.php
+++ api/src/Controller/LoginController.php
@@ -6,18 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Psr\Log\LoggerInterface;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils, LoggerInterface $logger): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
-
-        $logger->info('I just got the logger');
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
